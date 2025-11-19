c806859d9d5c1462a1188ef11c675623b5503637
Kidus Kibrom <kidusc941@gmail.com>
2025-11-19 16:07:28 +0300

    template impleenation is showing wrong

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -7,8 +7,7 @@ from . import views
 urlpatterns = [
         path("", views.home, name= "home"),
         path("books/", list_books, name = "list_books"),
-        path("library/<int:pk>/", views.LibraryDetailView.as_view(), name = "library_detail"),
-        
+        path("library/<int:pk>/", views.LibraryDetailView.as_view(), name = "library_detail"),  
 
         ]
 
--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,13 +1,10 @@
-from django.shortcuts import render # i don't know what this is
+from django.shortcuts import render 
 from .models import Library, Book
 from django.views.generic.detail import DetailView
-from django.http import HttpResponse
+
 
 
 # Create your views here.
-
-def home(request):
-    return HttpResponse("Welcome to the Library App!")
 
 
 def list_books(request):
@@ -18,5 +15,5 @@ def list_books(request):
 class LibraryDetailView(DetailView):
     model = Library
     template_name = "relationship_app/library_detail.html"
-    context_object_name = "library",
+    context_object_name = "library"
 
