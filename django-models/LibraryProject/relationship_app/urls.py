73e23e82842c5924637b8e12de456262ff0d9cff
Kidus Kibrom <kidusc941@gmail.com>
2025-11-19 15:26:37 +0300

    fixed the function based view to list_books and used the import

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,12 +1,12 @@
 from django.contrib import admin
 from django.urls import path
-from . views import list_books
+from .views import list_books
 from . import views
 
 
 urlpatterns = [
         path("", views.home, name= "home"),
-        path("books/", views.list_books, name = "list_books"),
+        path("books/", list_books, name = "list_books"),
         path("library/<int:pk>/", views.LibraryDetailView.as_view(), name = "library_detail"),
         
 
