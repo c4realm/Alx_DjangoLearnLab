22ace326daa7ffc0067797ab8d5fe179dfa9e374
Kidus Kibrom <kidusc941@gmail.com>
2025-12-21 22:44:57 +0300

    the migration worked

--- django-models/LibraryProject/relationship_app/models.py
+++ django-models/LibraryProject/relationship_app/models.py
@@ -5,17 +5,15 @@ from django.dispatch import receiver
 
 
 class UserProfile(models.Model):
-    ROLE_CHOICES = (
-        ('Admin', 'Admin'),
-        ('Librarian', 'Librarian'),
-        ('Member', 'Member'),
-    )
-
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    role = models.CharField(max_length=20, choices=ROLE_CHOICES)
-
-    def __str__(self):
-        return f"{self.user.username} - {self.role}"
+    role = models.CharField(
+        max_length=20,
+        choices=[
+            ('Admin', 'Admin'),
+            ('Librarian', 'Librarian'),
+            ('Member', 'Member'),
+        ]
+    )
 
 
 @receiver(post_save, sender=User)
--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -2,18 +2,17 @@ from django.shortcuts import render
 from django.contrib.auth.decorators import user_passes_test
 
 
-@user_passes_test(lambda u: hasattr(u, 'userprofile') and u.userprofile.role == 'Admin')
+@user_passes_test(lambda user: user.userprofile.role == 'Admin')
 def admin_view(request):
     return render(request, 'admin_view.html')
 
 
-@user_passes_test(lambda u: hasattr(u, 'userprofile') and u.userprofile.role == 'Librarian')
+@user_passes_test(lambda user: user.userprofile.role == 'Librarian')
 def librarian_view(request):
     return render(request, 'librarian_view.html')
 
 
-@user_passes_test(lambda u: hasattr(u, 'userprofile') and u.userprofile.role == 'Member')
+@user_passes_test(lambda user: user.userprofile.role == 'Member')
 def member_view(request):
     return render(request, 'member_view.html')
-
 
