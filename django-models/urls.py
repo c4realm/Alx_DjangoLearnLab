e44798752a698220314085e1e06fba76d7ee539c
Kidus Kibrom <kidusc941@gmail.com>
2025-12-21 23:01:11 +0300

    captializng the letters if it works

--- django-models/urls.py
+++ django-models/urls.py
@@ -2,8 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('admin-view/', views.admin_view, name='admin_view'),
-    path('librarian-view/', views.librarian_view, name='librarian_view'),
-    path('member-view/', views.member_view, name='member_view'),
+    path('Admin-view/', views.admin_view, name='Admin_view'),
+    path('Librarian-view/', views.librarian_view, name='Librarian_view'),
+    path('Member-view/', views.member_view, name='Member_view'),
 ]
 
--- django-models/views.py
+++ django-models/views.py
@@ -4,14 +4,14 @@ from django.contrib.auth.decorators import user_passes_test
 
 @user_passes_test(lambda u: hasattr(u, 'userprofile') and u.userprofile.role == 'Admin')
 def admin_view(request):
-    return render(request, 'admin_view.html')
+    return render(request, 'Admin_view.html')
 
 @user_passes_test(lambda u: hasattr(u, 'userprofile') and u.userprofile.role == 'Librarian')
 def librarian_view(request):
-    return render(request, 'librarian_view.html')
+    return render(request, 'Librarian_view.html')
 
 
 @user_passes_test(lambda u: hasattr(u, 'userprofile') and u.userprofile.role == 'Member')
 def member_view(request):
-    return render(request, 'member_view.html')
+    return render(request, 'Member_view.html')
 
