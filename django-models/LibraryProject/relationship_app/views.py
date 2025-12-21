ea796682223edb2a6c8ad89fa5bf9835a6502420
Kidus Kibrom <kidusc941@gmail.com>
2025-12-21 22:55:35 +0300

    fixing checker admin view for RBAC  milstone

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,18 +1,14 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import user_passes_test
 
-
 @user_passes_test(lambda user: user.userprofile.role == 'Admin')
 def admin_view(request):
     return render(request, 'admin_view.html')
-
 
 @user_passes_test(lambda user: user.userprofile.role == 'Librarian')
 def librarian_view(request):
     return render(request, 'librarian_view.html')
 
-
 @user_passes_test(lambda user: user.userprofile.role == 'Member')
 def member_view(request):
     return render(request, 'member_view.html')
-
