1271864f18ee8db9915789d71c08deb3c5e21c52
Kidus Kibrom <kidusc941@gmail.com>
2025-12-22 08:43:52 +0300

    persmission

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,17 +1,17 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import permission_required
 
-@permission_required('relationship_app.canaddbook', raise_exception=True)
+@permission_required('relationship_app.can_add_book', raise_exception=True)
 def add_book(request):
     return render(request, 'add_book.html')
 
 
-@permission_required('relationship_app.canchangebook', raise_exception=True)
+@permission_required('relationship_app.can_change_book', raise_exception=True)
 def edit_book(request, book_id):
     return render(request, 'edit_book.html')
 
 
-@permission_required('relationship_app.candeletebook', raise_exception=True)
+@permission_required('relationship_app.can_delete_book', raise_exception=True)
 def delete_book(request, book_id):
     return render(request, 'delete_book.html')
 
