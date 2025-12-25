af02a83f9d5e74593576c8d75ee6a95f75146fa8
Kidus Kibrom <kidusc941@gmail.com>
2025-12-25 22:29:15 +0300

    fixed the proper formating for the imports of modelviewset

--- api_project/api/views.py
+++ api_project/api/views.py
@@ -1,6 +1,6 @@
 # Create your views here.
 from rest_framework.generics import ListAPIView
-from rest_framework.viewsets import ModelViewSet
+from rest_framework import viewsets
 from .models import Book
 from .serializers import BookSerializer
 
@@ -8,7 +8,7 @@ class BookList(ListAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
 
-class BookViewSet(ModelViewSet):
+class BookViewSet(viewsets.ModelViewSet):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
 
