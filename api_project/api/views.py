7acb9d80032ca5fb01e338a1fc8c25d712b8c98c
Kidus Kibrom <kidusc941@gmail.com>
2025-12-25 22:24:32 +0300

    add CRUD operations using Viewset and Rounters

--- api_project/api/views.py
+++ api_project/api/views.py
@@ -1,9 +1,14 @@
 # Create your views here.
-from rest_framework import generics.ListAPIView
+from rest_framework.generics import ListAPIView
+from rest_framework.viewsets import ModelViewSet
 from .models import Book
 from .serializers import BookSerializer
 
 class BookList(ListAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+
+class BookViewSet(ModelViewSet):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
 
