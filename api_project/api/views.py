00930a5f0751fa2a0a2c8879b07549c304317a42
Kidus Kibrom <kidusc941@gmail.com>
2025-12-25 22:39:33 +0300

    Implementation of token authentication and permissions

--- api_project/api/views.py
+++ api_project/api/views.py
@@ -1,14 +1,20 @@
 # Create your views here.
+# Permissions are applied using DRF permission classes
+# Token authentication is enabled globally in settings.py
+
 from rest_framework.generics import ListAPIView
 from rest_framework import viewsets
+from rest_framework.permissions import IsAuthenticated, IsAdminUser
 from .models import Book
 from .serializers import BookSerializer
 
 class BookList(ListAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
+    permission_classes = [IsAuthenticated]
 
 class BookViewSet(viewsets.ModelViewSet):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
+    permission_classes = [IsAdminUser]
 
