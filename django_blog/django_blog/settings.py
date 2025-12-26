a66fe9852b316d851dc8e3ce8230a1db35f6742d
Kidus Kibrom <kidusc941@gmail.com>
2025-12-26 16:50:55 +0300

    implementing the blog user authenication system

--- django_blog/django_blog/settings.py
+++ django_blog/django_blog/settings.py
@@ -103,6 +103,8 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+LOGIN_REDIRECT_URL = "/profile/"
+LOGOUT_REDIRECT_URL = "/login/"
 
 # Internationalization
 # https://docs.djangoproject.com/en/5.2/topics/i18n/
