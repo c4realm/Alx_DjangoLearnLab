ba0b69be042d19791c08621a302487eb3461864d
Kidus Kibrom <kidusc941@gmail.com>
2025-12-28 13:21:51 +0300

    added the explcitly asked imports and methods calls

--- social_media_api/accounts/serializers.py
+++ social_media_api/accounts/serializers.py
@@ -1,19 +1,25 @@
+from django.contrib.auth import get_user_model
 from rest_framework import serializers
-from .models import User
+from rest_framework.authtoken.models import Token
+
+User = get_user_model()
+
 
 class RegisterSerializer(serializers.ModelSerializer):
     password = serializers.CharField(write_only=True)
 
     class Meta:
         model = User
-        fields = ['username', 'email', 'password', 'bio']
+        fields = ['username', 'email', 'password']
 
     def create(self, validated_data):
-        user = User.objects.create_user(
+        user = get_user_model().objects.create_user(
             username=validated_data['username'],
             email=validated_data.get('email'),
-            password=validated_data['password'],
-            bio=validated_data.get('bio', '')
+            password=validated_data['password']
         )
+
+        Token.objects.create(user=user)
         return user
+
 
