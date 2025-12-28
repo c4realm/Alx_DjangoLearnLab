29842000f9ca29526489094c3a03d5dcf3ab4222
Kidus Kibrom <kidusc941@gmail.com>
2025-12-28 05:08:08 +0300

    added tagwidger() explicity

--- django_blog/blog/forms.py
+++ django_blog/blog/forms.py
@@ -1,13 +1,19 @@
 from django import forms
 from .models import Post
 from .models import Comment
+from taggit.forms import TagWidget
 
 # to handles create and update forms safely
 
 class PostForm(forms.ModelForm):  #Modelform is explicity required, author will be automatiacally in view
     class Meta:
         model = Post
-        fields = ["title", "content"]
+        fields = ["title", "content", "tags"]
+        widgets = {
+
+                "tag": TagWidget(),
+
+                }
 
         #users only type the comment text, post and author are set automatically in view
 class CommentForm(forms.ModelForm):
